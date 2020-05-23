dac4cf5ad88c64204f1f5ff699c54c479edc8074
Hrituja Khatavkar <37546675+Hrituja@users.noreply.github.com>
2020-05-23 14:00:59 +0530

    Add files via upload

---
+++ LSTM/tf_data.py
@@ -0,0 +1,36 @@
+import pandas
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing import sequence
+from unidecode import unidecode
+from keras.utils.np_utils import to_categorical
+import numpy as np
+class TF_Data:
+    def __init__(self, data_file, validation_split=0.1, top_words=5000):
+        self.top_words = top_words
+        self.validation_split = validation_split
+        self.df = pandas.read_csv(data_file)
+
+        self.headlines = self.df['normalized_headline']
+        self.tokenizer = Tokenizer(top_words)
+        self.tokenizer.fit_on_texts(self.headlines)
+        self.all_x = self.tokenizer.texts_to_sequences(self.headlines)
+
+
+
+    def load_data(self, day='tomorrow'):
+        np.random.seed(0)
+        self.all_y = self.df[day]
+        idx = np.arange(len(self.all_x))
+        np.random.shuffle(idx)
+        self.all_x = np.array(self.all_x)[idx]
+        self.all_y = np.array(self.all_y)[idx]
+        split = int(self.validation_split * len(self.all_x))
+        training_x = self.all_x[split:]
+        training_y = self.all_y[split:]
+        validation_x = self.all_x[:split]
+        validation_y = self.all_y[:split]
+        np.random.seed(None)
+        return (training_x, training_y), (validation_x, validation_y)
+
+    def test_sentence(self, text):
+        return np.array(sequence.pad_sequences(self.tokenizer.texts_to_sequences([unidecode(text)]),maxlen=100))
---
+++ LSTM/tf_train.py
@@ -0,0 +1,71 @@
+# LSTM and CNN for sequence classification in the IMDB dataset
+import numpy
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers.convolutional import Conv1D
+from keras.layers.convolutional import MaxPooling1D
+from keras.layers.embeddings import Embedding
+from keras.preprocessing import sequence
+from keras.preprocessing.text import Tokenizer
+from keras.callbacks import ReduceLROnPlateau, ModelCheckpoint, EarlyStopping
+from tf_data import TF_Data
+import pandas
+from keras.models import load_model
+from os.path import isfile
+import os
+from keras.optimizers import Adam
+import pickle
+
+
+def train(filename, model_name, day='tomorrow'):
+    reduceLR = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=3, verbose=1, mode='auto')
+    checkpoint = ModelCheckpoint(model_name, monitor='val_acc', mode='auto', verbose=1, save_best_only=True)
+    earlyStopping = EarlyStopping(monitor='val_acc', min_delta=0, patience=5, verbose=1)
+    df = pandas.read_csv(filename)
+    print(df)
+    headlines = df['normalized_headline']
+    # print(headlines)
+    top_words = 2000
+
+    data = TF_Data(filename, top_words=top_words)
+    pickle.dump(data, open(filename.replace(".csv", ".p"), "wb"))
+    # load the dataset but only keep the top n words, zero the rest
+    (X_train, y_train), (X_test, y_test) = data.load_data(day=day)
+    print(X_train.shape)
+
+    # truncate and pad input sequences
+    max_review_length = 100
+    X_train = sequence.pad_sequences(X_train, maxlen=max_review_length)
+    X_test = sequence.pad_sequences(X_test, maxlen=max_review_length)
+    # create the model
+    embedding_vecor_length = 32
+    model = Sequential()
+    model.add(Embedding(top_words, embedding_vecor_length, input_length=max_review_length))
+    model.add(Conv1D(filters=32, kernel_size=3, padding='same', activation='relu'))
+    model.add(MaxPooling1D(pool_size=2))
+    model.add(LSTM(100))
+    model.add(Dense(1, activation='sigmoid'))
+    optimizer = Adam(lr=1e-3)
+    model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+    if isfile(model_name) and False:
+        print('Checkpoint Loaded')
+        model = save_model(model_name)
+    print(model.summary())
+
+    model.fit(X_train, y_train, validation_split=0.02, epochs=1, batch_size=64,
+              callbacks=[checkpoint, earlyStopping, reduceLR])
+    # Final evaluation of the model
+    model = load_model(model_name)
+    scores = model.evaluate(X_test, y_test, verbose=0)
+    fd = open('accuracy.csv', 'a')
+    CsvRow = [filename, day, "Accuracy: %.2f%%" % (scores[1] * 100)]
+    print(CsvRow)
+    fd.write(", ".join(CsvRow) + "\n")
+    fd.close()
+
+
+for f in os.listdir(r"../data"):
+    if (f.endswith('.csv')):
+        for d in ['ms_today', 'ms_tomorrow']:
+            train(os.path.join(r"../data", f), f.replace(".csv", "_" + d + "_.hdf5"), d)
